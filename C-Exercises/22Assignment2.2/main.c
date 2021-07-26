358c689c1d2d91102fe2b1f682a5b6a74db20b26
matheus costa <matheuscosta.tech@gmail.com>
2021-07-26 08:07:43 -0300

    Final version of assignment2.2

--- C-Exercises/22Assignment2.2/main.c
+++ C-Exercises/22Assignment2.2/main.c
@@ -5,6 +5,7 @@
 
 
 // ---------------------- GLOBALS --------------------------
+#define PLATE_LENGTH 8
 #define SPEED_LIMIT 60 // Km/h
 #define PAYED_PER_KM 5 // R$5.00 payed per km beyond the speed limit
 #define MAX_FINE_AMOUNT 190
@@ -17,7 +18,7 @@ typedef struct {
 
 struct HighSpeedFine {
 	int id;
-	char licensePlate[8];
+	char licensePlate[PLATE_LENGTH];
 	int speed;
 	Date *date;
 	int fineAmount;
@@ -65,40 +66,42 @@ struct HighSpeedFine* createNode() {
 	return node;
 }
 
+void printSpeedFine(struct HighSpeedFine *fine) {
+	printf("id%d: %s R$%d %dkm/h [%d/%d/%d] \n", fine->id, 
+								   	   			 fine->licensePlate,
+								   	   			 fine->fineAmount,
+									   			 fine->speed,
+									   			 fine->date->day,
+									   			 fine->date->month,
+									   			 fine->date->year);
+}
+
 void printLinkedList(struct LinkedList *list) {
 	/* Print list with format using square brackets */
 	if (isEmpty(list))
 		return;
 
 	struct HighSpeedFine *el = list->first;
-	printf("[");
 
 	do {
-		if (el->next != NULL) // If it's not the last, print with comma
-			printf("id%d: %s R$%d %dkm/h, \n", el->id, 
-								   	  		   el->licensePlate,
-								   	  		   el->fineAmount,
-									  		   el->speed);
-		else
-			printf("id%d: %s R$%d %dkm/h", el->id, 
-							   			   el->licensePlate,
-							   			   el->fineAmount,
-										   el->speed);
-		
+		printSpeedFine(el);
 		el = el->next;
 	} while (el != NULL);
-
-	printf("]\n");
 }
 
 
 // ---------------- SPEED FINE OPERATIONS ------------------
 void registerSpeedFine(struct HighSpeedFine *fine,
-						  char licensePlate[],
-						  int speed) {
+					   char licensePlate[],
+					   int speed,
+					   Date occurrenceDate) {
 	if (strlen(licensePlate) == 7 && speed > 0) {
 		strcpy(fine->licensePlate, licensePlate);
 		fine->speed = speed;
+
+		fine->date->day = occurrenceDate.day;
+		fine->date->month = occurrenceDate.month;
+		fine->date->year = occurrenceDate.year;
 	} else {
 		printf("Valor incorreto %s - %dkm/h\n", licensePlate, speed);
 	}
@@ -233,36 +236,54 @@ int main() {
 	struct LinkedList *list = malloc(sizeof(struct LinkedList));
 	init(list);
 
-	struct HighSpeedFine *newItem = createNode();
-	registerSpeedFine(newItem,  "1234abc", 59);
-	calculateFine(newItem);
-	insertFirst(list, newItem);
+	char plate[PLATE_LENGTH];
+	int speed = 0;
+	Date date;
 
-	newItem = createNode();
-	registerSpeedFine(newItem, "1234acb", 60);
-	calculateFine(newItem);
-	insertLast(list, newItem);
+	struct HighSpeedFine *newSpeedFine;
+	int op = 0;
 
-	newItem = createNode();
-	registerSpeedFine(newItem, "1234cba", 61);
-	calculateFine(newItem);
-	insertLast(list, newItem);
+	do {
+		// Validate entries for new speed fine
+		do {
+			printf("Digite a placa do veiculo[sete caracteres]: ");
+			scanf("%s", plate);
 
-	newItem = createNode();
-	registerSpeedFine(newItem, "1234cbb", 65);
-	calculateFine(newItem);
-	insertAt(list, newItem, 2);
+			if (strlen(plate) != PLATE_LENGTH-1)
+				puts("\nPlaca incorreta, digite novamente!");
+		} while(strlen(plate) != PLATE_LENGTH-1);
 
-	newItem = createNode();
-	registerSpeedFine(newItem, "1234bab", 98);
-	calculateFine(newItem);
-	insertLast(list, newItem);
+		do {
+			printf("Qual velocidade passou pelo semaforo: ");
+			scanf("%d", &speed);
+
+			if (speed == 0)
+				puts("O carro precisa estar andando, digite novamente!");
+			else if (speed < 0)
+				puts("O valor tem que ser positivo, digite novamente!");
+		} while(speed <= 0);
+
+		do {
+			printf("Qual a data da ocorrencia[DD MM AAAA]: ");
+			scanf("%d %d %d", &date.day, &date.month, &date.year);
+
+			if (date.day < 0 || date.month < 0 || date.year < 0)
+				puts("Invalido, data com valor negativo, tente novamente!");
+			else if (date.day == 0 || date.month == 0 || date.year == 0)
+				puts("Valor invalido, as datas comecam sempre com 1!");
+		} while(date.day <= 0 || date.month <= 0 || date.year <= 0);
+
+		newSpeedFine = createNode();
+		registerSpeedFine(newSpeedFine, plate, speed, date);
+		calculateFine(newSpeedFine);
+		insertLast(list, newSpeedFine);
+
+		printLinkedList(list);
+
+		printf("Registrar mais um? [1-sim/0-nao]: ");
+		scanf("%d", &op);
+	} while (op == 1);
 	
-	newItem = createNode();
-	registerSpeedFine(newItem, "1234cab", 99);
-	calculateFine(newItem);
-	insertLast(list, newItem);
-	printLinkedList(list);
 
 	free(list);
 	return 0;
