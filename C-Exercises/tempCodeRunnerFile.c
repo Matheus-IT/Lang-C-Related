558defd54eb6c5a79d0576179f97d822ab1aacd8
matheus costa <matheuscosta.tech@gmail.com>
2021-09-13 11:12:54 -0300

    Some questions in C

--- C-Exercises/tempCodeRunnerFile.c
+++ C-Exercises/tempCodeRunnerFile.c
@@ -1,1 +1,5 @@
-int i = 1; i <= n/2; i++
+rson *temp = queue->first;
+    // while (temp->next != NULL) {
+    //     printf("%s - %d\n", temp->name, temp->age);
+    //     temp = temp->next;
+    // }
