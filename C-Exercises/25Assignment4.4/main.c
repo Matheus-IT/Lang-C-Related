a580acbcaca848958f0d693e7802ff61af3fdd5b
matheus costa <matheuscosta.tech@gmail.com>
2021-09-22 16:09:09 -0300

    Working version of leaves removal

--- C-Exercises/25Assignment4.4/main.c
+++ C-Exercises/25Assignment4.4/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 
 struct Node {
@@ -22,21 +24,63 @@ Tree * createNewNode(int value) {
     return n;
 }
 
+bool isEmpty(Tree *root) {
+    return root == NULL;
+}
+
+Tree * insert(Tree *root, int value) {
+    if (isEmpty(root))
+        return createNewNode(value);
+
+    if (value > root->data)
+        root->subRight = insert(root->subRight, value);
+    else
+        root->subLeft = insert(root->subLeft, value);
+    return root;
+}
+
 Tree * populateTree(Tree *root, int samples[], unsigned length) {
     for (int i = 0; i < length; i++)
-        root = insert(samples[length]);
+        root = insert(root, samples[i]);
+    return root;
+}
+
+void inOrder(Tree *root) {
+    if (! isEmpty(root)) {
+        inOrder(root->subLeft);
+        printf("[%2i] ", root->data);
+        inOrder(root->subRight);
+    }
+}
+
+Tree * removeLeaves(Tree *root) {
+    if (isEmpty(root))
+        return NULL;
+
+    if (root->subLeft == NULL && root->subRight == NULL) {
+        free(root);
+        root = NULL;
+    }
+
+    if (! isEmpty(root)) {
+        root->subLeft = removeLeaves(root->subLeft);
+        root->subRight = removeLeaves(root->subRight);
+    }
     return root;
 }
 
 int main() {
-    const unsigned length = 12;
-    int vector[length] = {15, 16, 5, 20, 3, 12, 18, 13, 10, 23, 6, 7};
+    int vector[12] = {15, 16, 5, 20, 3, 12, 18, 13, 10, 23, 6, 7};
 
     Tree *root = init();
 
-    root = populateTree(root, vector, length);
+    root = populateTree(root, vector, 12);
 
-    // Next: print it...
+    inOrder(root);
+    printf("\n");
+    removeLeaves(root);
+    inOrder(root);
+
 
     return 0;
 }
